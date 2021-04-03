b03faf7e4d88e357011334977e706cf91aa6e7c5
khemsoth <kevhemsoth@gmail.com>
2021-04-03 09:23:53 -0400

    Header: nav-menu drops and retracts properly

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <input class="nav-btn" v-on:click="navToggle" :class="{ open: isOpen, closed: !isOpen }" type="button" value="+">
-    <div>
-      <nuxt-link to="/">Home</nuxt-link>
-      <nuxt-link to="/about">About</nuxt-link>
-      <nuxt-link to="/projects">Projects</nuxt-link>
-      <nuxt-link to="/contact">Contact</nuxt-link>
+    <div :class="{ 'nav-open': isOpen, 'nav-closed': !isOpen }"  class="nav">
+    <ul class="nav-list">
+      <li><nuxt-link class="nav-list-item" to="/">Home</nuxt-link></li>
+      <li><nuxt-link class="nav-list-item" to="/about">About</nuxt-link></li>
+      <li><nuxt-link class="nav-list-item" to="/projects">Projects</nuxt-link></li>
+      <li><nuxt-link class="nav-list-item" to="/contact">Contact</nuxt-link></li>
+    </ul>
     </div>
   </div>
 </template>
@@ -52,6 +54,46 @@ export default {
   .closed {
     transform: rotate(-90deg);
     transition-duration: .5s;
+  }
+
+  .nav-list {
+    list-style-type: none;
+    width: 15vw;
+    border: 1px solid black;
+
+  }
+
+  .nav-list-item {
+    text-decoration: none;
+  }
+
+  @keyframes menuDown {
+    0% {
+      transform: scaleY(0);
+    }
+    100% {
+      transform: scaleY(1);
+    }
+  }
+
+  @keyframes menuUp {
+    from {
+      transform: scaleY(1);
+    }
+    to {
+      transform: scaleY(0);
+    }
+  }
+
+  .nav-open {
+    animation: menuDown .5s ease-in-out forwards;
+    transform-origin: top;
+    visibility: visible
+  }
+
+  .nav-closed {
+    animation: menuUp .5s ease-in-out forwards;
+    transform-origin: top;
   }
 
 </style>
